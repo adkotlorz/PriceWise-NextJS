51125f18ed10328e1d490aa58d97974a688c1552
adkotlorz <adkotlorz@gmail.com>
2023-10-12 19:45:27 +0200

    hero section and searchbar ui ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,46 @@
+import Hero from "@/components/Hero";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Shopping made easy.
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary"> PriceWise</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            <Searchbar />
+          </div>
+          <Hero />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {/* Product card */}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
