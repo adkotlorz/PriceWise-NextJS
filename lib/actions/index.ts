5ecffcf58364c649c05dd8a8c61be92281ad6290
adkotlorz <adkotlorz@gmail.com>
2023-10-13 21:15:01 +0200

    modal for tracking price ready with email notif

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,7 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
 
 export const scrapeAndStoreProduct = async (productUrl: string) => {
   if (!productUrl) return;
@@ -86,6 +87,31 @@ export const getSimilarProducts = async (productId: string) => {
     }).limit(3);
 
     return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addUserEmailToProduct = async (
+  productId: string,
+  userEmail: string
+) => {
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) return;
+
+    const userExists = product.users.some(
+      (user: User) => user.email === userEmail
+    );
+
+    if (!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = generateEmailBody(product, "WELCOME");
+    }
   } catch (error) {
     console.log(error);
   }
