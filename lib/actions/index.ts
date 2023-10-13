cf15ced5a80e7d17452cf3e227e34a0ad86ee90b
adkotlorz <adkotlorz@gmail.com>
2023-10-13 17:46:39 +0200

    mongodb connection established

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,16 +1,17 @@
 "use server";
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export const scrapeAndStoreProduct = async (productUrl: string) => {
   if (!productUrl) return;
 
   try {
+    connectToDB();
+
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
     if (!scrapedProduct) return;
-
-    //DB here
   } catch (error: any) {
     throw new Error(`Failed to create product ${error.message}`);
   }
